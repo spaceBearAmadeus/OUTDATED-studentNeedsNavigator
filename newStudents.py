c97070d164693be9cb7f0e926782948fe3182209
spaceBearAmadeus (Alex) <alexanderpatrie@gmail.com>
2022-08-16 09:31:13 -0400

    deletion of old files
    I have switched my connector to SQLalchemy and have revised the structure of my program as such

--- newStudents.py
+++
@@ -1,2 +0,0 @@
-new_student_batch1 = [('Barak Obama', 'guitar', 300, 'T', 8, 'creative, personable', 4.5),
-                      ('Elon Musk', 'piano', 330, 't', 6, 'creative, redirection', 3)]      
